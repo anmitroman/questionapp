62fb0dc7d4fc25ddbb63ce155a834af4b3f06444
anmitroman <an.mankovskaya@yandex.ru>
2022-02-21 22:16:56 +0300

    Added remember me checkbox to new session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,17 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:email]
     if user&.authenticate(params[:password])
       sign_in(user)
+      remember(user) if params[:remember_me] == '1'
       flash[:success] = "Welcome, #{current_user.name_or_email}!!!"
       redirect_to root_path
     else
       flash[:warning] = "Incorrect email and/or password"
-      redirect_to new_session_path
+      render :new
     end
   end
 
   def destroy
+    forget current_user
     sign_out
     flash[:success] = "Good bye!"
     redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :old_password
+  attr_accessor :old_password, :remember_token
 
   has_secure_password validations: false
 
@@ -11,7 +11,29 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
   validate :password_complexity
   
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
+
+  def forget_me
+    self.remember_token = nil
+    update_column :remember_token_digest, nil
+  end
+
+  def remember_token_authenticated?(remember_token)
+    return false if remember_token_digest.present?
+    
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token) # digest from bd
+  end
+
   private
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.
+      min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$/
